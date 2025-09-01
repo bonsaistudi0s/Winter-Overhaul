fd3a26498109d17213aacf9c67a11798b8b0c0a3
Naz <bluspringindohq@gmail.com>
2025-09-02 02:52:15 +0800

    Set up sitting animation

--- common/src/main/java/tech/thatgravyboat/winteroverhaul/common/entity/ReplacedSnowGolem.java
+++ common/src/main/java/tech/thatgravyboat/winteroverhaul/common/entity/ReplacedSnowGolem.java
@@ -1,7 +1,9 @@
 package tech.thatgravyboat.winteroverhaul.common.entity;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import software.bernie.geckolib.animatable.GeoReplacedEntity;
+import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
@@ -11,14 +13,21 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 public class ReplacedSnowGolem implements GeoReplacedEntity {
     protected static final RawAnimation WALK = RawAnimation.begin().thenLoop("walk");
     protected static final RawAnimation IDLE = RawAnimation.begin().thenLoop("idle");
+    protected static final RawAnimation SIT = RawAnimation.begin().thenLoop("sit");
 
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
-    private static  <P extends GeoAnimatable> PlayState predicate(AnimationState<P> event) {
+    private static <P extends GeoAnimatable> PlayState predicate(AnimationState<P> event) {
         if (event.isMoving()) {
             event.getController().setAnimation(WALK);
         } else {
-            event.getController().setAnimation(IDLE);
+            Entity entity = event.getData(DataTickets.ENTITY);
+
+            if (entity.isPassenger() && entity.getVehicle() != null) {
+                event.getController().setAnimation(SIT);
+            } else {
+                event.getController().setAnimation(IDLE);
+            }
         }
         return PlayState.CONTINUE;
     }
