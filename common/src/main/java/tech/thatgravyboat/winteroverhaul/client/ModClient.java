5b78bdbc8ace64d2e9c49db168194058f9cf7c26
Naz <bluspringindohq@gmail.com>
2025-09-15 17:59:44 +0800

    Fix incorrect colour for skates on 1.21.1

--- common/src/main/java/tech/thatgravyboat/winteroverhaul/client/ModClient.java
+++ common/src/main/java/tech/thatgravyboat/winteroverhaul/client/ModClient.java
@@ -5,6 +5,7 @@ import dev.architectury.registry.client.particle.ParticleProviderRegistry;
 import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.component.DyedItemColor;
 import tech.thatgravyboat.winteroverhaul.client.particles.SnowflakeParticleProvider;
 import tech.thatgravyboat.winteroverhaul.client.renderer.entity.ReplacedSnowGolemRenderer;
 import tech.thatgravyboat.winteroverhaul.client.renderer.entity.RobinRenderer;
@@ -15,8 +16,11 @@ import tech.thatgravyboat.winteroverhaul.common.registry.ModParticles;
 public class ModClient {
     public static void onItemColors() {
         ColorHandlerRegistry.registerItemColors((stack, index) -> {
-            if (stack.has(DataComponents.DYED_COLOR)) {
-                return index == 0 ? stack.get(DataComponents.DYED_COLOR).rgb() : -1;
+            if (index == 0) {
+                if (stack.has(DataComponents.DYED_COLOR))
+                    return stack.get(DataComponents.DYED_COLOR).rgb();
+
+                return DyedItemColor.LEATHER_COLOR;
             }
             return -1;
         }, ModItems.SKATES.get());
