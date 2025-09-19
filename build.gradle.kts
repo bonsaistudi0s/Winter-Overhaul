699570c60241f0b4a110bff5291800374f442983
Naz <bluspringindohq@gmail.com>
2025-09-19 22:34:20 +0800

    Set up publishing via Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,32 @@
+import me.modmuss50.mpp.ModPublishExtension
+import me.modmuss50.mpp.ReleaseType
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
+import net.fabricmc.loom.task.RemapJarTask
 
 plugins {
     `java`
     id("architectury-plugin") version "3.4-SNAPSHOT"
     id("dev.architectury.loom") version "1.10-SNAPSHOT" apply false
     id("com.gradleup.shadow") version "8.3.8" apply false
+    id("me.modmuss50.mod-publish-plugin") version "0.7.+" apply false
+}
+
+val modVersion = createVersion()
+
+fun createVersion(): String {
+    // Override to use the GitHub tag from Releases
+    if (System.getenv("GITHUB_TAG") != null) {
+        return System.getenv("GITHUB_TAG").removePrefix("v")
+    }
+
+    return rootProject.property("mod_version") as String
 }
 
 allprojects {
     apply(plugin = "java")
     apply(plugin = "architectury-plugin")
 
-    version = "${rootProject.property("mod_version")}+${rootProject.property("minecraft_version")}"
+    version = "${modVersion}+${rootProject.property("minecraft_version")}"
     group = rootProject.property("maven_group") as String
 
     repositories {
@@ -49,5 +64,64 @@ subprojects {
     tasks.withType(JavaCompile::class.java) {
         options.encoding = "UTF-8"
         options.release.set(17)
+    }
+
+    if (project.path.contains("fabric") || project.path.contains("forge")) {
+        apply(plugin = "me.modmuss50.mod-publish-plugin")
+
+        val properLoaderName = when (project.property("loom.platform")) {
+            "fabric" -> "Fabric"
+            "forge" -> "Forge"
+            "neoforge" -> "NeoForge"
+            else -> ""
+        }
+
+        project.extensions.configure<ModPublishExtension>("publishMods") {
+            val mcVersion = rootProject.property("minecraft_version") as String
+
+            file = tasks.named<RemapJarTask>("remapJar").get().archiveFile
+            displayName = "[$properLoaderName] Winter Overhaul - $modVersion - $mcVersion"
+            version = "$modVersion+${mcVersion}-${project.property("loom.platform")}"
+            changelog = System.getenv("RELEASE_DESCRIPTION") ?: ""
+            type = ReleaseType.STABLE
+            modLoaders.add(project.property("loom.platform").toString())
+
+            dryRun = providers.environmentVariable("MODRINTH_TOKEN")
+                .getOrNull() == null || providers.environmentVariable("CURSEFORGE_TOKEN").getOrNull() == null
+
+            modrinth {
+                projectId = rootProject.property("publishing.modrinth").toString()
+                accessToken = providers.environmentVariable("MODRINTH_TOKEN")
+                minecraftVersions.addAll(rootProject.property("supported_versions").toString().split(","))
+                if (project.property("loom.platform") == "fabric") {
+                    requires {
+                        slug = "fabric-api"
+                    }
+                }
+                requires {
+                    slug = "architectury-api"
+                }
+                requires {
+                    slug = "geckolib"
+                }
+            }
+
+            curseforge {
+                projectId = rootProject.property("publishing.curseforge").toString()
+                accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
+                minecraftVersions.addAll(rootProject.property("supported_versions").toString().split(","))
+                if (project.property("loom.platform") == "fabric") {
+                    requires {
+                        slug = "fabric-api"
+                    }
+                }
+                requires {
+                    slug = "architectury-api"
+                }
+                requires {
+                    slug = "geckolib"
+                }
+            }
+        }
     }
 }
